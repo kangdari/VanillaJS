5023da05124d2c96cabcd00a6210ab900113cc68
kangdari_ <ksh44820@naver.com>
2020-04-17 14:01:47 +0900

    searchInput 포커싱 && 초기화

--- Search_Cats/src/SearchInput.js
+++ Search_Cats/src/SearchInput.js
@@ -1,12 +1,10 @@
 // 검색창 input
 class SearchInput {
   constructor({ $target, onSearch }) {
+    const header = document.querySelector('header')
     const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = "고양이를 검색...";
-
-    const header = document.querySelector('header')
-
     $searchInput.className = "SearchInput";
 
     // App에 자식 요소로 추가
@@ -14,6 +12,8 @@ class SearchInput {
     // $searchBlock.appendChild($searchInput);
 
     header.appendChild($searchInput)
+    // input에 focus 되도록 설정
+    $searchInput.focus();
 
     // 엔터를 누르면 onSearch 함수 수행
     $searchInput.addEventListener("keyup", (e) => {
@@ -23,6 +23,12 @@ class SearchInput {
         e.target.value = "";
       }
     });
+
+    // input 클릭 시 input 값 초기화
+    $searchInput.addEventListener('click', (e) => {
+      e.target.value = "";
+    })
+
     console.log("SearchInput creted", this);
   }
   render() {}
